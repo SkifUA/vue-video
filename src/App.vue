6e1176cc082a62a918294e4dcf69ea718f2b0763
Valerii Kuzin <kuzin.valeriy@gmail.com>
2021-03-14 00:02:26 +0200

    Implemented Searching in Youtube

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
 </template>
 
 <script>
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyChALGHb6aFHUjBo2I7qArOF4kWW9MlrhA';
 
 export default {
   name: 'App',
@@ -15,7 +17,16 @@ export default {
 },
   methods: {
     onTermChange(searchTerm) {
-      console.log(searchTerm);
+      axios
+          .get('https://www.googleapis.com/youtube/v3/search', {
+            params: {
+              key: API_KEY,
+              type: 'video',
+              part: 'snippet',
+              q: searchTerm
+            }
+          })
+          .then(response => console.log(response));
     }
   }
 }
